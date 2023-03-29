b6a5dc5e979d37bf3d31060caffad00d7559cfa2
Sieger06 <irek.haibulin@gmail.com>
2023-03-29 10:13:16 +0300

    1

---
+++ src/main/java/com/example/intromaven/IntroMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.intromaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intromaven/IntroMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intromaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
